9d522fb60e4f44e389fffa1945239f1e133e9a26
Mateusz Brawański <emzi0767@gmail.com>
2018-06-29 19:53:21 +0200

    Added license data and readme.

--- app/src/main/java/com/emzi0767/r6stratroulette/StrategyRouletteFragment.java
+++ app/src/main/java/com/emzi0767/r6stratroulette/StrategyRouletteFragment.java
@@ -1,5 +1,20 @@
-package com.emzi0767.r6stratroulette;
+// This file is a part of R6: Strat Roulette project.
+//
+// Copyright 2018 Emzi0767
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
+package com.emzi0767.r6stratroulette;
 
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -18,7 +33,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.stream.Collectors;
-
 
 /**
  * A simple {@link Fragment} subclass.
